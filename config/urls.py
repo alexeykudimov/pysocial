636b3550914171b8f9488b86865e6732c2cf1785
Alexey <work.alexeykudimov@gmail.com>
2021-08-17 20:11:56 +0300

    added media

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    # path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('src.routes'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
